7650480f93d8c747dba197f8bc8eab5153d5a576
vcato <vaughncato@gmail.com>
2017-07-15 10:35:12 -0400

    Made changes to cplusplus/sorting
    Trying to make it easier to see the equivalances in the algorithms.

--- cplusplus/sorting/impure_functions/main.cpp
+++ cplusplus/sorting/impure_functions/main.cpp
@@ -12,96 +12,102 @@ using iter = vector<int>::iterator;
 
 static ostream& operator<<(ostream& stream,const vector<int> &values)
 {
-  stream << "[";
-  bool first = true;
+    stream << "[";
+    bool first = true;
 
-  for (const auto x : values) {
-    if (!first) {
-      stream << ",";
+    for (const auto x : values) {
+        if (!first) {
+            stream << ",";
+        }
+        else {
+            first = false;
+        }
+        stream << x;
     }
-    else {
-      first = false;
-    }
-    stream << x;
-  }
 
-  stream << "]";
-  return stream;
+    stream << "]";
+    return stream;
 }
 
 
 static iter get_values_less_than(int pivot,iter range_start,iter range_end)
 {
-  iter read_iter = range_start;
-  iter write_iter = range_start;
+    iter read_iter = range_start;
+    iter write_iter = range_start;
 
-  while (read_iter != range_end) {
-    if (*read_iter < pivot) {
-      std::swap(*write_iter,*read_iter);
-      ++write_iter;
+    while (read_iter != range_end) {
+        if (*read_iter < pivot) {
+            std::swap(*write_iter,*read_iter);
+            ++write_iter;
+        }
+        ++read_iter;
     }
-    ++read_iter;
-  }
 
-  return write_iter;
+    return write_iter;
 }
 
 
 static iter get_values_equal_to(int pivot,iter range_start,iter range_end)
 {
-  iter read_iter = range_start;
-  iter write_iter = range_start;
+    iter read_iter = range_start;
+    iter write_iter = range_start;
 
-  while (read_iter != range_end) {
-    if (*read_iter == pivot) {
-      std::swap(*write_iter,*read_iter);
-      ++write_iter;
+    while (read_iter != range_end) {
+        if (*read_iter == pivot) {
+            std::swap(*write_iter,*read_iter);
+            ++write_iter;
+        }
+        ++read_iter;
     }
-    ++read_iter;
-  }
 
-  return write_iter;
+    return write_iter;
 }
 
 
-static void sort_range(iter range_begin,iter range_end)
+static void mysort_range(iter range_begin,iter range_end)
 {
-  if (range_end-range_begin<2) return;
+    if (range_end-range_begin<2) return;
 
-  int pivot = *(range_begin + (range_end-range_begin)/2);
+    int pivot = *(range_begin + (range_end-range_begin)/2);
 
-  iter equal_begin = get_values_less_than(pivot,range_begin,range_end);
-  iter equal_end = get_values_equal_to(pivot,equal_begin,range_end);
+    iter equal_begin = get_values_less_than(pivot,range_begin,range_end);
+    iter equal_end = get_values_equal_to(pivot,equal_begin,range_end);
 
-  sort_range(range_begin,equal_begin);
-  sort_range(equal_end,range_end);
+    iter lower_begin = range_begin;
+    iter lower_end = equal_begin;
+
+    iter upper_begin = equal_end;
+    iter upper_end = range_end;
+
+    mysort_range(lower_begin,lower_end);
+    mysort_range(upper_begin,upper_end);
 }
 
 
-static void test_sorted(const vector<int> &values,const vector<int> &expected)
+static void test_mysort(const vector<int> &values,const vector<int> &expected)
 {
-  vector<int> sorted_values = values;
-  sort_range(sorted_values.begin(),sorted_values.end());
-  assert(sorted_values==expected);
+    vector<int> sorted_values = values;
+    mysort_range(sorted_values.begin(),sorted_values.end());
+    assert(sorted_values==expected);
 }
 
 
 static void run_tests()
 {
-  test_sorted({},{});
-  test_sorted({1},{1});
-  test_sorted({1,2},{1,2});
-  test_sorted({2,1},{1,2});
-  test_sorted({1,2,3},{1,2,3});
-  test_sorted({1,3,2},{1,2,3});
+    test_mysort({},{});
+    test_mysort({1},{1});
+    test_mysort({1,2},{1,2});
+    test_mysort({2,1},{1,2});
+    test_mysort({1,2,3},{1,2,3});
+    test_mysort({1,3,2},{1,2,3});
 }
 
 
 int main()
 {
-  run_tests();
+    run_tests();
 
-  vector<int> sorted_values = {5,7,3,2,9};
-  sort_range(sorted_values.begin(),sorted_values.end());
-  cout << sorted_values << "\n";
+    vector<int> sorted_values = {5,7,3,2,9};
+    mysort_range(sorted_values.begin(),sorted_values.end());
+    cout << sorted_values << "\n";
 }
--- cplusplus/sorting/pure_functions/main.cpp
+++ cplusplus/sorting/pure_functions/main.cpp
@@ -12,19 +12,19 @@ using std::ostream;
 
 static ostream& operator<<(ostream& stream,const vector<int> &values)
 {
-  stream << "[";
-  bool first = true;
-  for (const auto x : values) {
-    if (!first) {
-      stream << ",";
+    stream << "[";
+    bool first = true;
+    for (const auto x : values) {
+        if (!first) {
+            stream << ",";
+        }
+        else {
+            first = false;
+        }
+        stream << x;
     }
-    else {
-      first = false;
-    }
-    stream << x;
-  }
-  stream << "]";
-  return stream;
+    stream << "]";
+    return stream;
 }
 
 
@@ -33,94 +33,96 @@ static ostream& operator<<(ostream& stream,const vector<int> &values)
 // Note that these functions are pure, even though they call the impure functions
 // like std::copy() and vector::push_back().
 namespace pure {
-  static vector<int> values_less_than(int pivot,const vector<int> &values)
-  {
-    vector<int> result;
+    static vector<int> values_less_than(int pivot,const vector<int> &values)
+    {
+        vector<int> result;
 
-    for (auto x : values) {
-      if (x<pivot) {
-	result.push_back(x);
-      }
+        for (auto x : values) {
+            if (x<pivot) {
+                result.push_back(x);
+            }
+        }
+
+        return result;
     }
 
-    return result;
-  }
 
+    static vector<int> values_equal_to(int pivot,const vector<int> &values)
+    {
+        vector<int> result;
 
-  static vector<int> values_equal_to(int pivot,const vector<int> &values)
-  {
-    vector<int> result;
+        for (auto x : values) {
+            if (x==pivot) {
+                result.push_back(x);
+            }
+        }
 
-    for (auto x : values) {
-      if (x==pivot) {
-	result.push_back(x);
-      }
+        return result;
     }
 
-    return result;
-  }
 
+    static vector<int> values_greater_than(int pivot,const vector<int> &values)
+    {
+        vector<int> result;
 
-  static vector<int> values_greater_than(int pivot,const vector<int> &values)
-  {
-    vector<int> result;
+        for (auto x : values) {
+            if (x>pivot) {
+                result.push_back(x);
+            }
+        }
 
-    for (auto x : values) {
-      if (x>pivot) {
-	result.push_back(x);
-      }
+        return result;
     }
 
-    return result;
-  }
-
-  static vector<int> concat(const vector<int> &a,const vector<int> &b)
-  {
-    vector<int> result(a.size()+b.size());
-    std::copy(a.begin(),a.end(),result.begin());
-    std::copy(b.begin(),b.end(),result.begin()+a.size());
-    return result;
-  }
+    static vector<int> concat(const vector<int> &a,const vector<int> &b)
+    {
+        vector<int> result(a.size()+b.size());
+        std::copy(a.begin(),a.end(),result.begin());
+        std::copy(b.begin(),b.end(),result.begin()+a.size());
+        return result;
+    }
 
 
-  static vector<int> concat(const vector<int> &a,const vector<int> &b,const vector<int> &c)
-  {
-    return concat(concat(a,b),c);
-  }
+    static vector<int>
+        concat(const vector<int> &a,const vector<int> &b,const vector<int> &c)
+    {
+        return concat(concat(a,b),c);
+    }
 
 
-  static vector<int> sorted(const vector<int> &values)
-  {
-    if (values.size()<2) return values;
+    static vector<int> mysorted(const vector<int> &values)
+    {
+        if (values.size()<2) return values;
 
-    size_t middle_index = values.size()/2;
-    int pivot = values[middle_index];
-    auto lower = sorted(values_less_than(pivot,values));
-    auto middle = values_equal_to(pivot,values);
-    auto upper = sorted(values_greater_than(pivot,values));
-    return concat(lower,middle,upper);
-  }
+        size_t middle_index = values.size()/2;
+        int pivot = values[middle_index];
+        auto lower = mysorted(values_less_than(pivot,values));
+        auto middle = values_equal_to(pivot,values);
+        auto upper = mysorted(values_greater_than(pivot,values));
+
+        return concat(lower,middle,upper);
+    }
 }
 
 
-static void test_sorted(const vector<int> &values,const vector<int> &expected)
+static void test_mysort(const vector<int> &values,const vector<int> &expected)
 {
-  assert(pure::sorted(values)==expected);
+    assert(pure::mysorted(values)==expected);
 }
 
 
 static void run_tests()
 {
-  test_sorted({},{});
-  test_sorted({1},{1});
-  test_sorted({1,2},{1,2});
-  test_sorted({2,1},{1,2});
-  test_sorted({1,2,3},{1,2,3});
-  test_sorted({1,3,2},{1,2,3});
+    test_mysort({},{});
+    test_mysort({1},{1});
+    test_mysort({1,2},{1,2});
+    test_mysort({2,1},{1,2});
+    test_mysort({1,2,3},{1,2,3});
+    test_mysort({1,3,2},{1,2,3});
 }
 
 int main()
 {
-  run_tests();
-  cout << pure::sorted({5,7,3,2,9}) << "\n";
+    run_tests();
+    cout << pure::mysorted({5,7,3,2,9}) << "\n";
 }
